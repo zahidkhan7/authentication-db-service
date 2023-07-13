09ded87e602c3b3c1203040782f1a75e7a2d3257
ZAHID KHAN <certainzkhan@gmail.com>
2023-07-13 11:06:08 +0300

    Initail file Committed

---
+++ src/main/java/com/zad/eng/excellence/vsa/AuthenticationDbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zad.eng.excellence.vsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationDbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationDbServiceApplication.class, args);
+	}
+
+}
